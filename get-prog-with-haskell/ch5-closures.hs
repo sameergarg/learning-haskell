c223c8647b358a339ab61bae4f0ef19df48a0142
sameer garg <garg.sameer@gmail.com>
2018-05-15 18:28:16 +0100

    chapter 5 exercises

---
+++ get-prog-with-haskell/ch5-closures.hs
@@ -0,0 +1,45 @@
+inc = (\x -> x + 1)
+
+double = (\x -> 2*x)
+
+applyIfEven fn = (\x -> if even x then fn x else x)
+
+ifEvenFn fn x = if even x then fn x else x
+
+ifEvenDouble x = ifEvenFn double x
+
+getRequestURL host apiKey resource id = host ++
+                                        "/" ++
+                                        resource ++
+                                        "/" ++
+                                        id ++
+                                        "?token=" ++
+                                        apiKey
+
+getHostRequestURL host = (\apiKey resource id -> getRequestURL host apiKey resource id)
+
+hostBuilder = (\host -> getHostRequestURL host)
+
+genApiRequestBuilder hostBuilder apiKey resource = (\id ->
+                                            hostBuilder apiKey resource id)
+apiRequestBuilder host =  genApiRequestBuilder getHostRequestURL host
+
+exampleBuilder = getRequestURL "http://example.com" "1337hAsk3ll" "books"
+
+-- QC5.4
+subtract2 = flip (-) 2
+
+-- Q 5.1
+
+ifEven fn x = if even x then fn x else x
+
+ifEvenInc = ifEven (\x -> x+1)
+
+binaryPartialApplication binFn = (\x y -> binFn x y)
+
+
+
+
+
+
+
